177a825d7ec328e4674ab06119e16f71d01dd02e
Kiuchi <trinsideu.m213@gmail.com>
2022-10-21 00:37:51 +0900

    version 0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "im-select-for-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('im-select-for-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from im-select for vscode!');
+	let disposable = vscode.commands.registerCommand('im-select-for-vscode.turnOffJapanese', () => {
+		switchJapaneseIME(false);
 	});
+	context.subscriptions.push(disposable);
 
+	disposable = vscode.commands.registerCommand('im-select-for-vscode.turnOnJapanese', () => {
+		switchJapaneseIME(true);
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.window.onDidChangeActiveTextEditor((_) => {
+		switchJapaneseIME(false);
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.window.onDidChangeWindowState((_) => {
+		switchJapaneseIME(false);
+	});
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function switchJapaneseIME(isOn: boolean) {
+	const zenhanExePath = vscode.workspace.getConfiguration().get<string>('im-select-for-vscode.cmdPath');
+	if (zenhanExePath) {
+		const args = isOn ? 'com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese' : 'com.apple.keylayout.ABC';
+		exec(`"${zenhanExePath}" ${args}`, (err, stdout, srderr) => { });
+	}
+}
+
+export function deactivate() { }
